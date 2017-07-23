0c2ca07e8492f05a196fa5a7075b76a3464214cd
vamsimanohar999 <vamsimanohar.business@gmail.com>
2017-07-23 10:08:37 +0530

    ad

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+ function setTimer(elem_id, date) {
+// set the date we're counting down to
+    var target_date = new Date(date).getTime();
+    // variables for time units
+    var days, hours, minutes, seconds;
+    // get tag element
+
+    var countDownElem = document.getElementById(elem_id);
+    //update the tag with id "countdown" every 1 second
+    setInterval(function () {
+       // find the  of "seconds" between now and target
+      var current_date = new Date().getTime();
+      var seconds_left = (target_date - current_date) / 1000;
+      // do some time calculations
+      days = parseInt(seconds_left / 86400);
+      seconds_left = seconds_left % 86400;
+      hours = parseInt(seconds_left / 3600);
+      seconds_left = seconds_left % 3600;
+      minutes = parseInt(seconds_left / 60);
+      seconds = parseInt(seconds_left % 60);
+      // format countdown string + set tag value
+      countDownElem.innerHTML = days + " D : " + hours + " H : " + minutes + " M : " + seconds + " S";
+    }, 1000);
+   }
+setTimer("countdown1","Aug 15, 2014");
+setTimer("countdown2","Aug 17, 2014");
+setTimer("countdown3","Aug 1, 2017");
 jQuery(function($) {'use strict';
 
 	//Responsive Nav
@@ -111,3 +138,4 @@ jQuery(function($) {'use strict';
 	}
 
 });
+
